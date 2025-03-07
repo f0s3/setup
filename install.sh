a18a117c73a426fe471cf8dc16dc5adff1cd6a7c
f0s3 <power.aerozoll@gmail.com>
2025-03-07 16:56:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 curl -sSfL https://raw.githubusercontent.com/f0s3/setup/refs/heads/main/config.json -o config.json
-# curl -sSfL https://raw.githubusercontent.com/f0s3/setup/refs/heads/main/creds.json -o creds.json
+curl -sSfL https://raw.githubusercontent.com/f0s3/setup/refs/heads/main/creds.json -o creds.json
 
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy archlinux-keyring --noconfirm
 pacman -S --noconfirm vim
 
-# vim creds.json && archinstall --config ./config.json --creds ./creds.json
-archinstall --config ./config.json
+vim creds.json && archinstall --config ./config.json --creds ./creds.json
